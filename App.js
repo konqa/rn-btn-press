ed1c6d60432c07aa8ba2900f6060a4c948aea5fe
Victor Bagu <bagu@Victors-MBP.home>
2020-11-06 22:10:31 +0200

    update button press

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "Batanayi", key: "1" },
+    { name: "Chingwa", key: "2" },
+    { name: "Nhamo", key: "3" },
+    { name: "Hondo", key: "4" },
+    { name: "Dambudzo", key: "5" },
+    { name: "Madzudzo", key: "6" },
+    { name: "Banbo", key: "7" },
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.key != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +46,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    backgroundColor: "pink",
+    padding: 30,
+    fontSize: 24,
   },
 });
